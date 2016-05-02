aef7a1c39bba285a2f72d8ffa92e936b4bfd6cda
Amadeo Asco <a.asco@bocaditos.co.uk>
2016-05-02 20:05:54 +0100

    Forgot to commit the Catalogue Service unit-tests

---
+++ src/test/java/com/aas/samples/customerproducts/service/CatalogueServiceJdbcTests.java
@@ -0,0 +1,20 @@
+package com.aas.samples.customerproducts.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * Integration test using the jdbc profile.</p>
+ *
+ * @author Amadeo Asco
+ * @see AbstratCatalogueServiceTests for more details.
+ */
+@ContextConfiguration(locations = {"classpath:spring/business-config.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("jdbc")
+public class CatalogueServiceJdbcTests extends AbstratCatalogueServiceTests {
+
+}
